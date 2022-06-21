5e17eb09935a4c84454b2b19a6444f5f11d7f836
jingzhen <jingzhen@hd123.com>
2022-06-21 13:41:08 +0800

    init

---
+++ edc-account/src/main/java/com/edcccd/account/SpringAccountApplication.java
@@ -0,0 +1,12 @@
+package com.edcccd.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAccountApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringAccountApplication.class);
+  }
+}
