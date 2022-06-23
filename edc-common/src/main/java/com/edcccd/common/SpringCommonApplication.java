3aac9c7fde46e2c9221e65bc2f9758374cab3ddd
jingzhen <jingzhen@hd123.com>
2022-06-23 17:47:39 +0800

    init

---
+++ edc-common/src/main/java/com/edcccd/common/SpringCommonApplication.java
@@ -0,0 +1,11 @@
+package com.edcccd.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCommonApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(SpringCommonApplication.class);
+  }
+}
